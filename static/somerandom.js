bfe9f05a680e5e27bee2affc8ffd1e27fcffd3ab
HrithvikVuppala <126831858+HrithvikVuppala@users.noreply.github.com>
2023-05-10 14:32:13 +0000

    Update somerandom.js

--- static/somerandom.js
+++ static/somerandom.js
@@ -1,5 +1,5 @@
  var password = window.localStorage.getItem('password');
-        var validPassword = "geforcenowaccount";
+        var validPassword = "nihalisanegro";
         if (password && password === validPassword) {
             document.body.style.display = 'block';
         } else {
