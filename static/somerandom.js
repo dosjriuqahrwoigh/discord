a80d5abcefb9a8ef3ba103dbef8ebec9b84a20a4
HrithvikVuppala <126831858+HrithvikVuppala@users.noreply.github.com>
2023-05-19 02:55:37 +0000

    Update somerandom.js

--- static/somerandom.js
+++ static/somerandom.js
@@ -1,17 +1,1 @@
- var password = window.localStorage.getItem('password');
-        var validPassword = "123password";
-        if (password && password === validPassword) {
-            document.body.style.display = 'block';
-        } else {
-            var input = prompt('Enter the password:');
-            if (input === validPassword) {
-                window.localStorage.setItem('password', input);
-                document.body.style.display = 'block';
-            } else {
-                alert('Invalid password.');
-                window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
-            }
-        }
-        if (document.body.style.display !== 'block') {
-            document.body.style.display = 'none';
-        } 
+var _0x1a8400=_0x4080;function _0x4b5f(){var _0x137d1a=['5697992oKpKZP','263896ephVaB','4rMowtW','31434601CpALpf','body','localStorage','href','none','2703156GunvyU','5476810DupKTp','block','style','834vNcqOX','Enter\x20the\x20password:','setItem','Invalid\x20password.','https://www.youtube.com/watch?v=dQw4w9WgXcQ','9UkZCQN','136830qLeZBn','13505pNLlbN','196WqbYam'];_0x4b5f=function(){return _0x137d1a;};return _0x4b5f();}(function(_0x4780c9,_0x1a8ef2){var _0x281a1a=_0x4080,_0x19de16=_0x4780c9();while(!![]){try{var _0x2a2232=-parseInt(_0x281a1a(0xa5))/0x1*(-parseInt(_0x281a1a(0xa0))/0x2)+-parseInt(_0x281a1a(0xab))/0x3+-parseInt(_0x281a1a(0xa3))/0x4+-parseInt(_0x281a1a(0xa1))/0x5*(parseInt(_0x281a1a(0x9a))/0x6)+-parseInt(_0x281a1a(0xa2))/0x7*(-parseInt(_0x281a1a(0xa4))/0x8)+parseInt(_0x281a1a(0x9f))/0x9*(-parseInt(_0x281a1a(0xac))/0xa)+parseInt(_0x281a1a(0xa6))/0xb;if(_0x2a2232===_0x1a8ef2)break;else _0x19de16['push'](_0x19de16['shift']());}catch(_0x283a84){_0x19de16['push'](_0x19de16['shift']());}}}(_0x4b5f,0xc4dad));var password=window[_0x1a8400(0xa8)]['getItem']('password'),validPassword='123password';function _0x4080(_0x21f429,_0x39c0a8){var _0x4b5f8d=_0x4b5f();return _0x4080=function(_0x4080a5,_0x46ea8f){_0x4080a5=_0x4080a5-0x99;var _0x535848=_0x4b5f8d[_0x4080a5];return _0x535848;},_0x4080(_0x21f429,_0x39c0a8);}if(password&&password===validPassword)document[_0x1a8400(0xa7)][_0x1a8400(0x99)]['display']='block';else{var input=prompt(_0x1a8400(0x9b));input===validPassword?(window[_0x1a8400(0xa8)][_0x1a8400(0x9c)]('password',input),document[_0x1a8400(0xa7)][_0x1a8400(0x99)]['display']=_0x1a8400(0xad)):(alert(_0x1a8400(0x9d)),window['location'][_0x1a8400(0xa9)]=_0x1a8400(0x9e));}document[_0x1a8400(0xa7)][_0x1a8400(0x99)]['display']!=='block'&&(document['body'][_0x1a8400(0x99)]['display']=_0x1a8400(0xaa));
