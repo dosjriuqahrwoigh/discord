20369262933e5b9fc234271ee9e4168197ff6f8f
HrithvikVuppala <126831858+HrithvikVuppala@users.noreply.github.com>
2023-05-20 06:14:57 +0000

    Update js.js

--- static/js/js.js
+++ static/js/js.js
@@ -287,7 +287,7 @@ function skipAd() {
 let newTab = (url, uxor = true) => {
 	// creates a new hypertab!!
 	chromeTabs.addTab({
-		title: 'New Tab - Join: bit.ly/DscrdServer',
+		title: 'New Tab | Join: bit.ly/DscrdServer',
 	});
 
 	URL_BAR.value = '';
