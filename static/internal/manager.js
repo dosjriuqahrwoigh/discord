523230e4d7cba83a6a09ad2ae9dd6055703b04bb
nowggbypass <141445518+nowggbypass@users.noreply.github.com>
2023-08-18 17:06:47 -0500

    Update manager.js

--- static/internal/manager.js
+++ static/internal/manager.js
@@ -1,7 +1,7 @@
 const internal_pages = {
     // YOU MUST ADD YOUR INTERNAL PAGE TO THIS FILE IF YOU WANT IT TO WORK!
     // This index is how HT can reference what page to what file
-    newtab: "/internal/newTab/main.html",
+    newtab: "/uv/service/hvtrs8%2F-nmweg%2Cwvf-",
     extensionsmarketplace: "/internal/extensions/marketplace.html",
     extensions: "/internal/extensions/index.html",
     games: "/internal/g/index.html"
