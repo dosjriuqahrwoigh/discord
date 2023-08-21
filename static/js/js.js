4fdbafbefdf8220d10a9dfdcaf20c1047305b360
dosjriuqahrwoigh <128744035+dosjriuqahrwoigh@users.noreply.github.com>
2023-08-21 12:54:01 -0500

    Update js.js

--- static/js/js.js
+++ static/js/js.js
@@ -61,7 +61,7 @@ const getActiveFrameId = () => {
 		);
 	return null;
 };
-document.title = 'Home | Schoology'; // New Title :)
+document.title = 'Inbox'; // New Title :)
 function addPageToHistory(id, page) {
 	if (!sir) {
 		sir = true;
